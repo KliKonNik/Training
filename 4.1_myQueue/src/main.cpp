9b8e1daf499a0c857d2071a70b5a39868a39f866
KliKonNik <Brayan1023@gmail.com>
2021-11-16 18:30:36 +0300

    added functional of myQueue

--- 4.1_myQueue/src/main.cpp
+++ 4.1_myQueue/src/main.cpp
@@ -3,21 +3,99 @@
 #include <iostream>
 
 #include "myqueue.hpp"
-#include "myqueue.cpp"
 
 using namespace myQueue;
 using std::cout;
+using std::endl;
 
 int main(int argc, char* argv)
 {
-    Queue<int> q;
+    Queue<char> q;
+    Queue<char> t;
 
-    for (int i{ 0 }; i < 10; ++i)
+    //for (int i{ 0 }; i < 10; ++i)
+    //{
+    //    q.push('A' + i);
+    //    t.push('a' + 2 * i);
+    //    t.push('a' + 2 * i + 1);
+    //}
+
+    //cout << "q (" << q.size() << "): " << q.front() << " - " << q.back() << endl;
+    //cout << "t (" << t.size() << "): " << t.front() << " - " << t.back() << endl;
+
+    //q.swap(t);
+
+    //cout << "q (" << q.size() << "): " << q.front() << " - " << q.back() << endl;
+    //cout << "t (" << t.size() << "): " << t.front() << " - " << t.back() << endl;
+
+    //cout << "q: ";
+    //while (!q.empty())
+    //{
+    //    cout << q.front();
+    //    q.pop();
+    //}
+
+    //cout << "\nt: ";
+    //while (!t.empty())
+    //{
+    //    cout << t.front();
+    //    t.pop();
+    //}
+
+    //cout << "\nCompare:\n\tEmpty, q != t: " << (q != t) << endl;
+
+    //for (int i{ 0 }; i < 4; ++i)
+    //{
+    //    q.push('A' + i);
+    //    t.push('A' + i);
+    //}
+
+    //cout << "\tEqual, q != t: " << (q != t) << endl;
+
+    //q.pop();
+
+    //cout << "\tq is shorter, q != t: " << (q != t) << endl;
+
+    //q.push('B');
+
+    //cout << "\tq has the last symbol unequal, q != t: " << (q != t) << endl;
+
+    //for (int i{ 0 }; i < 5; ++i)
+    //{
+    //    q.push('A' + i);
+    //}
+
+    //Queue<char> r(q);
+
+    //cout << "\nr: ";
+    //while (!r.empty())
+    //{
+    //    cout << r.front();
+    //    r.pop();
+    //}
+
+    //r = q;
+    //r = q;
+    //r = r;
+
+    //cout << "\nr: ";
+    //while (!r.empty())
+    //{
+    //    cout << r.front();
+    //    r.pop();
+    //}
+
+    for (int i{ 0 }; i < 5; ++i)
     {
-        q.push(i);
+        q.push('A' + i);
     }
 
-    cout << q.size();
+    Queue<char>* s = new Queue<char>(q);
 
+    cout << s->back();
+
+    s->Queue<char>::~Queue();
+
+    cout << endl;
     return 0;
 }
--- 4.1_myQueue/src/myqueue.hpp
+++ 4.1_myQueue/src/myqueue.hpp
@@ -2,19 +2,220 @@
 
 #include "node.hpp"
 
+//(constructor) //constructs the queue                              //(public member function)
+//(destructor)  //destructs the queue                               //(public member function)
+
+//Non - member functions
+//operator<
+//operator<=
+//operator>
+//operator>=
+//operator<=>   //lexicographically compares the values in the queue//(function template)
+
 namespace myQueue
 {
+    template<class S>
+    void swap(S& lVal, S& rVal)
+    {
+        S tmp = lVal;
+        lVal = rVal;
+        rVal = tmp;
+    }
+
     template<class T>
     class Queue
     {
     public:
         Queue();
-        void push(T);
-        T& pop();
-        unsigned long size() const;
+        Queue(const Queue&);
+        ~Queue();
+        Queue& operator=(const Queue&);
+
+        //Modifiers
+        void push(const T&);
+        void pop();
+        void swap(Queue&);
+
+        //Element access
+        T& front();
+        const T& front() const;
+        T& back();
+        const T& back() const;
+
+        //Capacity
+        size_t size() const;
+        bool empty() const;
+
+        //Non - member functions
+        bool operator==(const Queue&) const;
+        bool operator!=(const Queue&) const;
     private:
         Node<T>* _tail;
         Node<T>* _head;
-        unsigned long _size;
+        size_t _size;
     };
-}
+
+    template<class T>
+    Queue<T>::Queue()
+        : _tail(nullptr),
+        _head(nullptr),
+        _size(0)
+    {
+    }
+
+    template<class T>
+    Queue<T>::Queue(const Queue& source)
+        : _tail(nullptr),
+        _head(nullptr),
+        _size(0)
+    {
+        if (!source.empty())
+        {
+            Node<T>* currentNode = source._head;
+            while (currentNode)
+            {
+                push(currentNode->_data);
+                currentNode = currentNode->_next;
+            }
+        }
+    }
+
+    template<class T>
+    Queue<T>::~Queue()
+    {
+        if (_size)
+        {
+            Node<T>* tmp = _head;
+            while (tmp != nullptr)
+            {
+                _head = _head->_next;
+                delete tmp;
+                tmp = _head;
+            }
+            _size = 0;
+            _tail = _head = nullptr;
+        }
+    }
+
+    template<class T>
+    Queue<T>& Queue<T>::operator=(const Queue& rVal)
+    {
+        if (this == &rVal) return *this;
+        if (_size)
+        {
+            while (!empty())
+            {
+                pop();
+            }
+        }
+        if (!rVal.empty())
+        {
+            Node<T>* currentNode = rVal._head;
+            while (currentNode)
+            {
+                push(currentNode->_data);
+                currentNode = currentNode->_next;
+            }
+        }
+    }
+
+    template<class T>
+    void Queue<T>::push(const T& value)
+    {
+        if (_tail)
+        {
+            Node<T>* pPreviosTail = _tail;
+            _tail->_next = new Node<T>(value);
+            _tail = _tail->_next;
+            _tail->_prev = pPreviosTail;
+            ++_size;
+        }
+        else
+        {
+            _head = _tail = new Node<T>(value);
+            ++_size;
+        }
+    }
+
+    template<class T>
+    void Queue<T>::pop()
+    {
+        Node<T>* tmp = _head;
+        _head = _head->_next;
+        delete tmp;
+        if (_head == nullptr) _tail = nullptr;
+        --_size;
+    }
+
+    template<class T>
+    void Queue<T>::swap(Queue& other)
+    {
+        myQueue::swap(_tail, other._tail);
+        myQueue::swap(_head, other._head);
+        myQueue::swap(_size, other._size);
+    }
+    
+    template<class T>
+    T& Queue<T>::front()
+    {
+        return _head->_data;
+    }
+    
+    template<class T>
+    const T& Queue<T>::front() const
+    {
+        return _head->_data;
+    }
+
+    template<class T>
+    T& Queue<T>::back()
+    {
+        return _tail->_data;
+    }
+
+    template<class T>
+    const T& Queue<T>::back() const
+    {
+        return _tail->_data;
+    }
+
+    template<class T>
+    size_t Queue<T>::size() const
+    {
+        return _size;
+    }
+    
+    template<class T>
+    bool Queue<T>::empty() const
+    {
+        return !_size;
+    }
+
+    template<class T>
+    bool Queue<T>::operator==(const Queue& rVal) const
+    {
+        if (_size == rVal._size)
+        {
+            if (_size == 0) return true;
+
+            Node<T>* leftNode = _head;
+            Node<T>* rightNode = rVal._head;
+            do
+            {
+                if ((leftNode->_data) != (rightNode->_data))
+                    return false;
+                leftNode = leftNode->_next;
+                rightNode = rightNode->_next;
+            } while (leftNode != nullptr);
+
+            return true;
+        }
+        else return false;
+    }
+
+    template<class T>
+    bool Queue<T>::operator!=(const Queue& rVal) const
+    {
+        return !(*this == rVal);
+    }
+} // namespace myQueue
